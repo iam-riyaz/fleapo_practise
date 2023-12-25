71b42705d41504c4db94048174e1fb37b7df138b
iam-riyaz <97458162+iam-riyaz@users.noreply.github.com>
2023-12-25 13:54:24 +0530

    timer app is done

--- timer/src/components/Timer.jsx
+++ timer/src/components/Timer.jsx
@@ -1,65 +1,53 @@
-import React, { useState, useEffect, useRef } from 'react';
+import { useEffect, useRef, useState } from "react";
 
 export const Timer = () => {
-  const [hours, setHours] = useState(0);
-  const [minutes, setMinutes] = useState(0);
   const [seconds, setSeconds] = useState(0);
-  const timerRef= useRef()
-  const [isActive,setIsActive]= useState(false)
+  const [minutes, setMinutes] = useState(0);
+  const [hours, setHours] = useState(0);
+  const [isActive, setIsActive] = useState(false);
+  const timerRef = useRef();
 
+  const timerStart = () => {
+    setIsActive(true);
+  };
+  const timerStop = () => {
+    setIsActive(false);
+  };
 
-  const timerStart=()=>{
-    setIsActive(true)
-  }
-  const timerStop=()=>{
-    setIsActive(false)
-  }
-
-  const timerReset=()=>{
-    setIsActive(false)
-    setSeconds(0)
-    setMinutes(0)
-    setHours(0)
-  }
+  const timerReset = () => {
+    setIsActive(false);
+    setSeconds(0);
+    setHours(0);
+    setMinutes(0);
+  };
 
   useEffect(() => {
+    if (isActive) {
+      timerRef.current = setInterval(() => {
+        setSeconds((pre) => (pre + 1) % 60);
+        if (seconds == 59) {
+          setMinutes((pre) => (pre + 1) % 60);
+        }
+        if (seconds == 59 && minutes == 59) {
+          setHours((pre) => pre + 1);
+        }
+      }, 1000);
+    } else {
+      clearInterval(timerRef.current);
+    }
 
-if(isActive){
-
-    timerRef.current = setInterval(() => {
-      // Update seconds
-      setSeconds((prevSeconds) => (prevSeconds + 1) % 60);
-    
-      // Update minutes
-      if (seconds === 59) {
-        setMinutes((prevMinutes) => (prevMinutes + 1) % 60);
-      }
-    
-      // Update hours
-      if (minutes === 59 && seconds === 59) {
-        setHours((prevHours) => prevHours + 1);
-      }
-    }, 1000);
-}
-else{
-    clearInterval(timerRef.current)
-}
-
-
-    // Cleanup interval on component unmount
-    return () => clearInterval(timerRef.current);
-  }, [isActive]);
+    return ()=>clearInterval(timerRef.current);
+  }, [isActive,seconds,minutes,hours]);
 
   return (
-    <div>
-      <p id='timer'>{String(hours).padStart(2, '0')}:{String(minutes).padStart(2, '0')}:{String(seconds).padStart(2, '0')}</p>
-      <div>
-        <button onClick={timerStart}>start</button>
-        <button onClick={timerStop}>stop</button>
-        <button onClick={timerReset}>reset</button>
-      </div>
-    </div>
+    <>
+      <p id="timer">
+        {String(hours).padStart(2, "0")}:{String(minutes).padStart(2, "0")}:
+        {String(seconds).padStart(2, "0")}
+      </p>
+      <button onClick={timerStart}>Start</button>
+      <button onClick={timerStop}>Stop</button>
+      <button onClick={timerReset}>Reset</button>
+    </>
   );
 };
-
-
